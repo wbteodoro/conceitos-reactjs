10f87be42da53b30e24087177911dcb536158aaa
Wellington.BT <w.b.teodoro@gmail.com>
2020-08-18 13:33:55 -0300

    Criando funcionalidade no Frontend conectando com a API, possibilitando listar, adicionar e remover os itens armazenados no backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+
+    api.get('repositories').then(({ data }) => {
+      console.log("data: ", data)
+      setRepositories(data);
+    });
+
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    api.post('repositories', {
+      title: "App Mobile",
+      url: "http://github.com/app-mobile",
+      techs: ["NodeJS", "React Native"]
+    }).then(({ data }) => {
+
+      setRepositories([ ...repositories, data ])
+    });
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+      const newRepositories = [...repositories];
+      const removedRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+      newRepositories.splice(removedRepositoryIndex, 1);
+
+      setRepositories(newRepositories);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
